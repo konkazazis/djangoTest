707aa3885a2d2171a00fe2e2d504e768ae353647
konkazazis <nikos_kazazis@yahoo.com>
2023-02-12 20:09:42 +0200

    adding admin functionality

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig'
+    'pages.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
