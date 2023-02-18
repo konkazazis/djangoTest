c8ec65794851f47cd33ef0f9d03bc9607b46fd65
konkazazis <nikos_kazazis@yahoo.com>
2023-02-19 01:56:48 +0200

    added timestamp and footer buttons

--- pages/models.py
+++ pages/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-from django.urls import reverse
-
+from django.urls import reverse   
 class Post(models.Model):
+    timestamp = models.DateTimeField(auto_now_add= True)
+    updated= models.DateTimeField(auto_now= True)
+    
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         "auth.User",
         on_delete=models.CASCADE,
     )
     text = models.TextField()
+
+    def __str__(self):
+        return self.timestamp
+        
+    def __str__(self):
+        return self.updated
 
     def __str__(self):
         return self.title
